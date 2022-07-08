fd10b92f2ed8eaa7eeaccc23c254debad4a4c653
Nikita Zelenkov <zelenkov4462@gmail.com>
2022-07-08 14:38:04 +0700

    pagination

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,24 +3,34 @@ import "./main.less";
 import { useDispatch, useSelector } from "react-redux";
 import { getRepos } from "../actions/repos";
 import Repo from "./repo/Repo";
+import { setCurrentPage } from "../../reducers/reposReducer";
+import { createPages } from "../../utils/pagesCreator";
 
 const Main = () => {
   const dispatch = useDispatch();
   const repos = useSelector((state) => state.repos.items);
   const isFetching = useSelector((state) => state.repos.isFetching);
+  const currentPage = useSelector((state) => state.repos.currentPage);
+  const totalCount = useSelector((state) => state.repos.totalCount);
+  const perPage = useSelector((state) => state.repos.perPage);
   const [searchValue, setSearchValue] = useState("");
+  const pagesCount = Math.ceil(totalCount / perPage);
+
+  const pages = [];
+  createPages(pages, pagesCount, currentPage);
 
   useEffect(() => {
-    dispatch(getRepos());
-  }, []);
+    dispatch(getRepos(searchValue, currentPage, perPage));
+  }, [currentPage]);
 
   const searchHandler = () => {
-    dispatch(getRepos(searchValue));
+    dispatch(setCurrentPage(1));
+    dispatch(getRepos(searchValue, currentPage, perPage));
   };
 
   const onKeyDown = (e) => {
     if (e.key === "Enter") {
-      dispatch(getRepos(searchValue));
+      dispatch(getRepos(searchValue, currentPage, perPage));
     }
   };
   return (
@@ -48,6 +58,17 @@ const Main = () => {
       ) : (
         <div className="fetching"></div>
       )}
+      <div className="pages">
+        {pages.map((page, index) => (
+          <span
+            onClick={() => dispatch(setCurrentPage(page))}
+            key={index}
+            className={currentPage === page ? "currentPage" : "page"}
+          >
+            {page}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
--- src/reducers/reposReducer.js
+++ src/reducers/reposReducer.js
@@ -1,9 +1,13 @@
 const SET_REPOS = "SET_REPOS";
 const SET_IS_FETCHING = "SET_IS_FETCHING";
+const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
 
 const defaultState = {
   items: [],
   isFetching: true,
+  currentPage: 1,
+  perPage: 10,
+  totalCount: 0,
 };
 
 export default function rootReducer(state = defaultState, action) {
@@ -12,12 +16,18 @@ export default function rootReducer(state = defaultState, action) {
       return {
         ...state,
         items: action.payload.items,
+        totalCount: action.payload.total_count,
         isFetching: false,
       };
     case SET_IS_FETCHING:
       return {
         ...state,
         isFetching: action.payload,
+      };
+    case SET_CURRENT_PAGE:
+      return {
+        ...state,
+        currentPage: action.payload,
       };
     default:
       return state;
@@ -28,4 +38,8 @@ export const setRepos = (repos) => ({ type: SET_REPOS, payload: repos });
 export const setIsFetching = (bool) => ({
   type: SET_IS_FETCHING,
   payload: bool,
+});
+export const setCurrentPage = (page) => ({
+  type: SET_CURRENT_PAGE,
+  payload: page,
 });
